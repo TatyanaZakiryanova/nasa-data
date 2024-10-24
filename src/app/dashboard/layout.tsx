571072d6809f78a497b628c3c54930ea77be8d33
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-10-24 21:12:54 +0300

    added navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,10 @@
+import Navbar from '../shared/navbar';
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="text-customTextColor mx-2 mt-5 flex-1 p-3">{children}</main>
+      <Navbar />
+      <main className="mx-2 mt-5 flex-1 p-3 text-customTextColor">{children}</main>
     </div>
   );
 }
---
+++ src/app/shared/navbar.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const linkClasses =
+    'flex items-center gap-2 rounded-lg p-3 text-sm text-white transition-all duration-300 hover:bg-customButtonHover';
+
+  return (
+    <nav className="flex items-center justify-between rounded-lg bg-customBackground p-4 shadow-md">
+      <ul className="flex gap-2">
+        <li>
+          <Link
+            href="/dashboard"
+            className={`${linkClasses} ${
+              pathname === '/dashboard' ? 'bg-customButton font-bold' : ''
+            }`}
+          >
+            Photo of the day
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/dashboard/search"
+            className={`${linkClasses} ${
+              pathname === '/dashboard/search'
+                ? 'bg-customButton font-bold text-white'
+                : 'text-white'
+            }`}
+          >
+            Search
+          </Link>
+        </li>
+      </ul>
+      <ul className="flex items-center gap-2">
+        <li>
+          <Link href="/" className={`${linkClasses}`}>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={`${linkClasses} ${
+              pathname === '/' ? 'bg-customButton font-bold text-white' : 'text-white'
+            }`}
+          >
+            Home
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
