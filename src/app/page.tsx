bb54f4045b2939da7a5a297abda8b540805b9c3f
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-11-01 01:38:45 +0300

    added login icons to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { Telescope } from 'lucide-react';
+import { LogIn, Telescope, UserPlus } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center bg-customBackground p-12">
+    <main className="relative flex h-screen flex-col items-center bg-customBackground p-12">
+      <aside className="absolute bottom-8 right-4 top-4 flex max-h-5 gap-4 text-sm text-customTextColor">
+        <Link href="/dashboard/registration" className="inline-flex justify-center gap-2">
+          <UserPlus size={15} /> Sign Up
+        </Link>
+        <Link href="/dashboard/login" className="inline-flex justify-center gap-2">
+          <LogIn size={15} /> Sign In
+        </Link>
+      </aside>
       <h1 className="gradient-text text-5xl font-bold tracking-wide sm:text-6xl md:text-7xl lg:text-8xl">
         NASA Data
       </h1>
