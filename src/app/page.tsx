81e8795b9106c1584a2c6bf185bc2bc424ec7607
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-11-01 15:26:43 +0300

    added buttons to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import { LogIn, Telescope, UserPlus } from 'lucide-react';
 import Link from 'next/link';
+import Button from './ui/button';
 
 export default function Home() {
   return (
     <main className="relative flex h-screen flex-col items-center bg-customBackground p-12">
-      <aside className="absolute bottom-8 right-4 top-4 flex max-h-5 gap-4 text-sm text-customTextColor">
-        <Link href="/dashboard/registration" className="inline-flex justify-center gap-2">
-          <UserPlus size={15} /> Sign Up
-        </Link>
-        <Link href="/dashboard/login" className="inline-flex justify-center gap-2">
-          <LogIn size={15} /> Sign In
-        </Link>
+      <aside className="absolute bottom-8 right-2 top-2 flex max-h-5 gap-2 text-sm text-customTextColor">
+        <Button className="flex items-center justify-center p-4">
+          <Link href="/dashboard/registration" className="inline-flex justify-center gap-1">
+            <UserPlus size={15} /> Sign Up
+          </Link>
+        </Button>
+        <Button className="flex items-center justify-center p-4">
+          <Link href="/dashboard/login" className="inline-flex justify-center gap-1">
+            <LogIn size={15} /> Sign In
+          </Link>
+        </Button>
       </aside>
-      <h1 className="gradient-text text-5xl font-bold tracking-wide sm:text-6xl md:text-7xl lg:text-8xl">
+      <h1 className="gradient-text m-5 text-5xl font-bold tracking-wide sm:text-6xl md:text-7xl lg:text-8xl">
         NASA Data
       </h1>
       <span className="text-sm text-white sm:text-base md:text-lg">
