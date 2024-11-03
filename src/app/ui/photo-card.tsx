03e1ee253f63fcb80bbd90e0da55214a0eeba28c
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-11-03 19:24:39 +0300

    dded favorite icon and styles

--- src/app/ui/photo-card.tsx
+++ src/app/ui/photo-card.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { FavoritePhoto } from '../redux/favorites/types';
 import { addToFavorites, removeFromFavorites } from '../redux/favorites/favoritesSlice';
+import { Heart, Sparkles, Star } from 'lucide-react';
 
 interface PhotoCardProps {
   id: string;
@@ -38,7 +39,7 @@ const PhotoCard: React.FC<PhotoCardProps> = React.memo(
 
     return (
       <div
-        className="m-2.5 w-[350px] cursor-pointer rounded-[5px] bg-customBackground p-[5px] text-center transition-shadow transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+        className="group relative m-2.5 w-[350px] cursor-pointer rounded-[5px] bg-customBackground p-[5px] text-center transition-shadow transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
         onClick={onClick}
       >
         <h3 className="mb-1 text-xs">{title}</h3>
@@ -51,17 +52,27 @@ const PhotoCard: React.FC<PhotoCardProps> = React.memo(
               style={{ borderRadius: '5px', objectFit: 'cover' }}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
+            <div
+              className={`absolute right-2 top-2 opacity-0 transition-opacity duration-500 group-hover:opacity-100 ${isFavorite ? 'opacity-100' : ''}`}
+            >
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  isFavorite ? handleRemoveFromFavorites() : handleAddToFavorites();
+                }}
+              >
+                <Star
+                  size={25}
+                  color="white"
+                  strokeWidth={1.25}
+                  className="transition-all duration-500 hover:scale-125"
+                />
+              </button>
+            </div>
           </div>
         )}
         {copyright && <span className="text-[10px]">{copyright}</span>}
         <span className="text-[10px]">{date}</span>
-        {isFavorite ? (
-          <button className="text-white" onClick={handleRemoveFromFavorites}>
-            Удалить из избранного
-          </button>
-        ) : (
-          <button onClick={handleAddToFavorites}>Добавить в избранное</button>
-        )}
       </div>
     );
   },
