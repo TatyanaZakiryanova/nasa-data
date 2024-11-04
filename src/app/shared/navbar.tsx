de16802f9a632be0dd0bc0d0e0428b47081a7354
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-11-04 21:47:47 +0300

    added burger menu for navbar

--- src/app/shared/navbar.tsx
+++ src/app/shared/navbar.tsx
@@ -5,99 +5,116 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { useAuth } from '../contexts/auth-context';
+import { useState } from 'react';
 
 export default function Navbar() {
   const pathname = usePathname();
   const { user, logout } = useAuth();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   const linkClasses =
     'flex items-center gap-2 rounded-lg p-3 text-sm text-white transition-all duration-300 hover:bg-customButtonHover';
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="flex items-center justify-between rounded-lg bg-customBackground p-4 shadow-md">
-      <ul className="flex gap-2">
-        <li>
-          <Link
-            href="/dashboard"
-            className={`${linkClasses} ${
-              pathname === '/dashboard' ? 'bg-customButton font-bold' : ''
-            }`}
-          >
-            <Camera size={20} />
-            Photo of the day
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/dashboard/search"
-            className={`${linkClasses} ${
-              pathname === '/dashboard/search'
-                ? 'bg-customButton font-bold text-white'
-                : 'text-white'
-            }`}
-          >
-            <Search size={20} />
-            Search
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex items-center gap-2">
-        {user ? (
-          <>
+    <nav className="relative flex items-center justify-between rounded-lg bg-customBackground p-4 shadow-md">
+      <button onClick={toggleMenu} className="lg:hidden">
+        <span className="text-white">☰</span>
+      </button>
+      <div
+        className={`w-full lg:flex lg:justify-between ${isMenuOpen ? 'absolute left-0 right-0 top-full z-10 block w-full rounded-lg bg-customBackground' : 'hidden'} lg:block`}
+      >
+        <ul className="flex flex-col lg:flex-row lg:gap-2">
+          <li>
             <Link
-              href="/dashboard/profile"
+              href="/dashboard"
               className={`${linkClasses} ${
-                pathname === '/dashboard/profile'
+                pathname === '/dashboard' ? 'bg-customButton font-bold' : ''
+              }`}
+            >
+              <Camera size={20} />
+              Photo of the day
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dashboard/search"
+              className={`${linkClasses} ${
+                pathname === '/dashboard/search'
                   ? 'bg-customButton font-bold text-white'
                   : 'text-white'
               }`}
             >
-              <CircleUser size={20} />
-              Profile
+              <Search size={20} />
+              Search
             </Link>
-            <button onClick={logout} className={`${linkClasses}`}>
-              <LogOut size={20} />
-              Sign Out
-            </button>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link
-                href="/dashboard/registration"
-                className={`${linkClasses} ${pathname === '/dashboard/registration' ? 'bg-customButton font-bold text-white' : 'text-white'}`}
-              >
-                <UserPlus size={20} />
-                Sign Up
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/dashboard/login"
-                className={`${linkClasses} ${
-                  pathname === '/dashboard/login'
-                    ? 'bg-customButton font-bold text-white'
-                    : 'text-white'
-                }`}
-              >
-                <LogIn size={20} />
-                Sign In
-              </Link>
-            </li>
-          </>
-        )}
-        <li>
-          <Link
-            href="/"
-            className={`${linkClasses} ${
-              pathname === '/' ? 'bg-customButton font-bold text-white' : 'text-white'
-            }`}
-          >
-            <House size={20} />
-            Home
-          </Link>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <ul className="flex flex-col lg:mt-0 lg:flex-row lg:gap-2">
+          {user ? (
+            <>
+              <li>
+                <Link
+                  href="/dashboard/profile"
+                  className={`${linkClasses} ${
+                    pathname === '/dashboard/profile'
+                      ? 'bg-customButton font-bold text-white'
+                      : 'text-white'
+                  }`}
+                >
+                  <CircleUser size={20} />
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <button onClick={logout} className={`${linkClasses}`}>
+                  <LogOut size={20} />
+                  Sign Out
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link
+                  href="/dashboard/registration"
+                  className={`${linkClasses} ${pathname === '/dashboard/registration' ? 'bg-customButton font-bold text-white' : 'text-white'}`}
+                >
+                  <UserPlus size={20} />
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/dashboard/login"
+                  className={`${linkClasses} ${
+                    pathname === '/dashboard/login'
+                      ? 'bg-customButton font-bold text-white'
+                      : 'text-white'
+                  }`}
+                >
+                  <LogIn size={20} />
+                  Sign In
+                </Link>
+              </li>
+            </>
+          )}
+          <li>
+            <Link
+              href="/"
+              className={`${linkClasses} ${
+                pathname === '/' ? 'bg-customButton font-bold text-white' : 'text-white'
+              }`}
+            >
+              <House size={20} />
+              Home
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
