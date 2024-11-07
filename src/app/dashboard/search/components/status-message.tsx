2ceea355fd5427ad83d6550dd5cd2055f4d42cd3
TatyanaZakiryanova <elenastrell@yandex.ru>
2024-11-07 03:54:12 +0300

    status-message media queries

--- src/app/dashboard/search/components/status-message.tsx
+++ src/app/dashboard/search/components/status-message.tsx
@@ -16,19 +16,25 @@ export default function StatusMessage({
   totalItems,
 }: StatusMessageProps) {
   if (!isSearched && status !== Status.SUCCESS && status !== Status.LOADING) {
-    return <p className="flex justify-center">Search for amazing space photos provided by NASA:</p>;
+    return (
+      <p className="sm:text-md flex justify-center text-sm">
+        Search for amazing space photos provided by NASA:
+      </p>
+    );
   }
 
   if (status === Status.ERROR) {
-    return <p className="my-2.5 text-center text-xl">Unable to complete the request</p>;
+    return <p className="sm:text-md my-2.5 text-center text-sm">Unable to complete the request</p>;
   }
 
   if (isSearched && photos.length === 0 && status !== Status.LOADING) {
-    return <p className="my-2.5 text-center text-xl">No photos were found for this request</p>;
+    return (
+      <p className="sm:text-md my-2.5 text-center text-sm">No photos were found for this request</p>
+    );
   }
 
   if (photos.length > 0) {
-    return <p className="my-2.5 text-center text-sm">Results found: {totalItems}</p>;
+    return <p className="sm:text-md my-2.5 text-center text-sm">Results found: {totalItems}</p>;
   }
   return null;
 }
