ca21f478941e29386338005e8416dd7689a89bc0
TatyanaZakiryanova <elenastrell@yandex.ru>
2025-10-26 01:42:29 +0300

    fix any type

--- src/app/main/search/page.tsx
+++ src/app/main/search/page.tsx
@@ -1,3 +1,5 @@
+import { ApiResponse } from '@/app/redux/photos/types';
+
 import ClientSearch from './components/client-search';
 import { Photo } from './types';
 
@@ -28,9 +30,9 @@ export default async function SearchPage() {
   );
 
   if (!res.ok) throw new Error('Failed to fetch data');
-  const data = await res.json();
+  const data: ApiResponse = await res.json();
 
-  const initialPhotos: Photo[] = data.collection.items.map((item: any) => ({
+  const initialPhotos: Photo[] = data.collection.items.map((item) => ({
     nasa_id: item.data[0].nasa_id,
     title: item.data[0].title,
     description: item.data[0].description,
